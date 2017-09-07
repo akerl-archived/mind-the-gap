aacb5ad66cc89b5343d599069c85aca4b7418ae3
Les Aker <me@lesaker.org>
2017-09-06 20:35:06 -0400

    clean up vault finder

--- 2_setup/3_shell/build.sh
+++ 2_setup/3_shell/build.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-echo '[[-f ~/.bashrc ]] && source ~/.bashrc' > ~/.profile
+echo '[[ -f ~/.bashrc ]] && source ~/.bashrc' > ~/.profile
 
 cp bashrc ~/.bashrc
 
-VAULT_UUID="$(lsblk -s -d -n -o UUID -I 253)"
+VAULT_UUID="$(lsblk -f | awk '/crypto_LUKS/ { print $3 }')"
 echo "export VAULT_UUID=$VAULT_UUID" > ~/.vault_uuid
 
+echo "Enter the root password"
+passwd
